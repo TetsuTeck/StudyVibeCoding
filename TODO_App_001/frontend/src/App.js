2f4477703408dd2e7de506ee68efc9ea60ff1a7c
natetsu8128 <tetsuya-nakatani@outolook.jp>
2025-07-27 16:59:23 +0900

    Feature: Implement dark mode functionality with theme context

--- TODO_App_001/frontend/src/App.js
+++ TODO_App_001/frontend/src/App.js
@@ -1,13 +1,33 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import TodoList from './components/TodoList';
 import TodoDetail from './components/TodoDetail';
+import { ThemeContext } from './contexts/ThemeContext';
 import './App.css';
 
 function App() {
+  const { theme, toggleTheme } = useContext(ThemeContext);
+
   return (
     <Router>
       <div className="App">
+        <nav className="navbar navbar-expand-lg bg-body-tertiary">
+          <div className="container-fluid">
+            <a className="navbar-brand" href="#">TODO App</a>
+            <div className="form-check form-switch ms-auto">
+              <input
+                className="form-check-input"
+                type="checkbox"
+                id="flexSwitchCheckDefault"
+                checked={theme === 'dark'}
+                onChange={toggleTheme}
+              />
+              <label className="form-check-label" htmlFor="flexSwitchCheckDefault">
+                Dark Mode
+              </label>
+            </div>
+          </div>
+        </nav>
         <Routes>
           <Route path="/" element={<TodoList />} />
           <Route path="/todos/:id" element={<TodoDetail />} />
